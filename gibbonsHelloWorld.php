3d4a0c3ef00546ca558b125823717989c053bc04
crgibbonsgtcc <55026339+crgibbonsgtcc@users.noreply.github.com>
2019-09-08 12:06:42 -0400

    Add files via upload

---
+++ gibbonsHelloWorld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<title>Hello World</title>
+	</head>
+	<body>
+
+	<?php 
+        
+    echo "<h1>Hello World!</h1>"; 
+    
+    ?>
+
+	</body>
+	</html>
+
+<!--I think I could've used either one in this case but I used double quotes so you can insert variables directly within the text of the string. With single quotes the text appears as it is. Single quotes are much stricter, It ignores all your variables.-->
---
+++ gibbonscarpayoff.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<title>Casey Gibbons Favorite Car</title>
+	</head>
+	<body>
+
+	<?php 
+        
+        $year = "1966";
+        $make = "Ford";
+        $model ="Mustang";
+        
+        echo "<b>My favorite car is a $year $make $model.</b>";
+        
+        echo "<br>";
+        echo "<br>";
+        
+        $cost = "70,000";
+        
+        echo "It cost approximately $cost.";
+        
+        echo "<br>";
+        echo "<br>";
+        
+        $x = 70000;  
+        $y = 12;
+
+        echo "If I dont have to pay interest it would take". " " . $x / $y. " " . "a month to pay the car off in a year.";
+        /*I divided $x and $y to get the sum amount*/
+        
+        echo "<br>";
+        echo "<br>";
+        
+        $a = 70000;  
+        $b = 24;
+
+        echo "If I dont have to pay interest it would take"." ". $a / $b. " " . "a month to pay the car off in 2 years.";
+        /*I divided $a and $b to get the sum amount*/
+    
+    ?>
+
+	</body>
+	</html>
